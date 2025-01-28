8417d8ef47fdda06195335a8396787d55a8dc947
berkay öztürk <berkay533434@gmail.com>
2025-01-28 19:51:26 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MenuProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
